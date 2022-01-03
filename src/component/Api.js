2452e06f1123e02890c069ebd0e575fbf1db5854
Gopinath R <teamx.aathisoft@gmail.com>
2022-01-03 12:12:58 +0530

    updates by gopi

--- src/component/Api.js
+++ src/component/Api.js
@@ -1,9 +1,33 @@
 import React from "react";
 import User from "./browser";
 import { useEffect, useState } from 'react';
+import axios from "axios";
 export default function Api(props){
    let UserList="";
    const [employees, setEmployees] = useState([]);
+
+  useEffect(()=>{
+    debugger;
+     // POST request using axios with set headers
+     const article1 = { title: 'React POST Request Example' };
+     const article = {
+      "id":1000,
+      "name":"senthillkumar",
+      "email":"abc@gmail.com",
+      "body":"welcome to aathisoft.com"
+
+   };
+     const headers = { 
+         'Authorization': 'Bearer 777a89a3f6edb99788713f0b4ffa1cb2d7f240ed6b9eaf58297d51b64e1c6423',
+         'My-Custom-Header': 'foobar'
+     };
+     axios.post('https://gorest.co.in/public/v1/posts/123/comments', article, { headers })
+         .then(response => {
+           console.log(response);
+            //this.setState({ articleId: response.data.id });
+         });
+  });
+
     return(
         <div>
             <FetchUsers/>
