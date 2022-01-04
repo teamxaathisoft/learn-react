de2c6f4b6c2e543dfda8dc9b6986e3f56bd78f2f
SenthilKumar P <sentthill@gmail.com>
2022-01-04 03:58:33 -0800

    update

--- src/Apipushdata/postform.js
+++ src/Apipushdata/postform.js
@@ -120,7 +120,6 @@ export default function Postform(props) {
       </div>
     <div>
     <button onClick={(event) => handleSubmit(event)}> Submit </button>
-
       </div>     
      
     </form>
